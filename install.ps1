8633e7aa0e430b0a39aa76a6e4cc1da6d567e3c7
CodingBoson <CodingBoson@proton.me>
2025-06-14 05:24:30 +0300

    Fix path in install.sh

--- install.ps1
+++
@@ -1,4 +0,0 @@
-# Build and install ChatGPTExtractor on Windows
-
-dotnet pack -c Release ChatGPTExtractor.csproj
-dotnet tool install --global --add-source .\bin\nupkg ChatGPTExtractor --version 1.0.0
--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # Build and install ChatGPTExtractor on Linux/OSX
 
 dotnet pack -c Release ChatGPTExtractor.csproj
-dotnet tool install --global --add-source .\bin\nupkg ChatGPTExtractor --version 1.0.0
+dotnet tool install --global --add-source ./bin/nupkg ChatGPTExtractor --version 1.0.0
